e9e6a3a20be26372f1bdb4e33f091195e846689c
Ahlam Mohammed <ahlam.m.alfashq@gmail.com>
2021-03-11 22:53:54 +0300

    first commit

---
+++ javascript/JavaScript.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    Scrolling0();
+   
+
+   
+});
+//------------------------------------------------------------
+function Scrolling0() {
+    $('a[href^="#"]').on('click', function (event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+
+    });
+}
+
